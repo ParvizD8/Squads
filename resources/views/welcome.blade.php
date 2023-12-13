c7eb30c98d2f55094151737f5280497fc97011e9
Dodoev Parvizjon <dparik9898@gmail.com>
2023-12-13 17:44:54 +0500

    Создание проекта

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+
+<title>Laravel From Scratch Blog</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+
+    .clamp {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+    
+    .clamp.one-line {
+        -webkit-line-clamp: 1;;
+    }
+</style>
+
+<body style="font-family: Open Sans, sans-serif">
+    <section class="px-6 py-8">
+        <nav class="md:flex md:justify-between md:items-center">
+            <div>
+                <a href="/">
+                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                </a>
+            </div>
+
+            <div class="mt-8 md:mt-0 flex items-center">
+                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                    <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
+            </div>
+        </nav>
+
+        {{ $slot }}
+
+        <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+            <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+
+            <div class="mt-10">
+                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
+
+                    <form method="POST" action="/newsletter" class="lg:flex text-sm">
+                        @csrf
+                    </form>
+                </div>
+            </div>
+        </footer>
+    </section>
+</body>
